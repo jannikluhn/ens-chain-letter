7975cb77c1373b91b2d78d0a30a51f4d2f9f5059
Jannik Luhn <jannik.luhn@posteo.de>
2023-04-04 11:41:54 +0200

    Move footer to bottom

--- src/routes/stamps/+page.svelte
+++ src/routes/stamps/+page.svelte
@@ -7,23 +7,27 @@
   export let data;
 </script>
 
-<Title />
-<h2 class="text-white text-6xl text-center mt-8 mb-16">Stamp Collection</h2>
-<div class="container mx-auto grid grid-cols-2 gap-4 md:grid-cols-4 mb-16">
-  {#each data.stamps.reverse() as stamp}
-    <div class="h-fit">
-      <Link href="/stamp/{stamp.tokenID}">
-        <img src={stamp.svgUrl} alt="Stamp #{stamp.tokenID}" />
-        <h2 class="text-white text-center text-2xl w-full mx-auto">
-          <ColorfulText
-            text="#{stamp.tokenID}"
-            colorIndex={stamp.tokenID - 1}
-            rainbow={false}
-            underlineOnHover={true}
-          />
-        </h2>
-      </Link>
+<div class="flex flex-col h-screen justify-between">
+  <div>
+    <Title />
+    <h2 class="text-white text-6xl text-center mt-8 mb-16">Stamp Collection</h2>
+    <div class="container mx-auto grid grid-cols-2 gap-4 md:grid-cols-4 mb-16">
+      {#each data.stamps.reverse() as stamp}
+        <div class="h-fit">
+          <Link href="/stamp/{stamp.tokenID}">
+            <img src={stamp.svgUrl} alt="Stamp #{stamp.tokenID}" />
+            <h2 class="text-white text-center text-2xl w-full mx-auto">
+              <ColorfulText
+                text="#{stamp.tokenID}"
+                colorIndex={stamp.tokenID - 1}
+                rainbow={false}
+                underlineOnHover={true}
+              />
+            </h2>
+          </Link>
+        </div>
+      {/each}
     </div>
-  {/each}
+  </div>
+  <Footer />
 </div>
-<Footer />
